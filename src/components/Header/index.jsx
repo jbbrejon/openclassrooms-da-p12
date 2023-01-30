3cf4e4f88469941569b46d1325b58defe8fa1043
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-01-30 13:58:57 +0100

    :construction: Create Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+// Import CSS module
+import styles from '../..//styles/Header.module.css'
+
+function Header() {
+
+    return (
+        <header>
+
+        </header>
+    )
+}
+
+export default Header
