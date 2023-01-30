d14b624a0138aeefde3750649a85ac013c7f84c4
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-01-30 14:24:31 +0100

    :sparkles: Add elements to component (logo, nav)

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,25 @@
+// Import Logo
+import logo from '../../assets/logo/logo.svg'
+
 // Import CSS module
 import styles from '../..//styles/Header.module.css'
 
 function Header() {
 
     return (
-        <header>
-
-        </header>
+        <>
+            <header className={styles.header}>
+                <img className={styles.logo} src={logo} alt="logo Sportsee" />
+                <nav className={styles.nav}>
+                    <ul className={styles.ul}>
+                        <li className={styles.li}>Accueil</li>
+                        <li className={styles.li}>Profil</li>
+                        <li className={styles.li}>Réglage</li>
+                        <li className={styles.li}>Communauté</li>
+                    </ul>
+                </nav>
+            </header>
+        </>
     )
 }
 
