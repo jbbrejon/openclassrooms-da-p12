1ad82264f158e025d6e49c8ca11f88b1f99c1b5c
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-01-31 19:22:37 +0100

    :construction: Set data from API to local state

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,12 +1,43 @@
+import Sidebar from '../../components/Sidebar'
+
 // Import CSS module
 import styles from '../../styles/User.module.css'
 
+import { useParams } from 'react-router-dom'
+
+import { useState, useEffect } from 'react';
+
+import fetchData from '../../api/fetchData/index.js';
+
 function User() {
 
-    return (
-        <div>
+    const { id } = useParams();
+    const idInt = parseInt(id);
+    const [userData, setUserData] = useState({})
+    const [isDataLoading, setDataLoading] = useState(false)
+    const [error, setError] = useState(false)
 
-        </div>
+    useEffect(() => {
+        async function getApi() {
+            setDataLoading(true)
+            try {
+                const data = await fetchData(idInt)
+                setUserData(data)
+            } catch (err) {
+                setError(true)
+
+            } finally {
+                setDataLoading(false)
+            }
+        }
+        getApi()
+    }, [idInt])
+    console.log(userData)
+    return (
+        <main className={styles.main}>
+            <Sidebar></Sidebar>
+            <div>Bonjour {userData.userInfos.firstName}  </div>
+        </main>
     )
 }
 
