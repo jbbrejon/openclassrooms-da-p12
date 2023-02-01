9e6399d3e61509459651d7b24679749c0d7515cd
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-02-01 11:50:47 +0100

    :construction: Create component

---
+++ src/components/Count/index.jsx
@@ -0,0 +1,9 @@
+export default function Count({ name, value, unit }) {
+    return (
+        <>
+            <div >
+                Name: {name}, Value: {value}, Unit: {unit}
+            </div>
+        </>
+    )
+}
