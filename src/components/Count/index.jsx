1429c789cde7e4f7a335d9079a7721c0a2227845
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-02-01 20:16:34 +0100

    :construction: Add elements

--- src/components/Count/index.jsx
+++ src/components/Count/index.jsx
@@ -1,8 +1,16 @@
-export default function Count({ name, value, unit }) {
+import styles from '../../styles/Count.module.css'
+
+export default function Count({ icon, name, cat, value, unit }) {
     return (
         <>
-            <div >
-                Name: {name}, Value: {value}, Unit: {unit}
+            <div className={styles.count}>
+                <div className={`${styles.icon} ${styles[cat]}`}>
+                    <img className={styles.caticon} src={icon} alt="activity" />
+                </div>
+                <div className={styles.info}>
+                    <div className={styles.data}>{value.toLocaleString("en-US")}{unit}</div>
+                    <div className={styles.name}>{name}</div>
+                </div>
             </div>
         </>
     )
