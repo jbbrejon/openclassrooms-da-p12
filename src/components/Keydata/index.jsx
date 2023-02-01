53780ab7e5ee938879ee76dce3fd1e1058a39cec
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-02-01 20:17:03 +0100

    :construction: Add elements

--- src/components/Keydata/index.jsx
+++ src/components/Keydata/index.jsx
@@ -1,11 +1,20 @@
 import Count from "../Count"
 
+import energy from '../../assets/keydata/energy.svg'
+import chicken from '../../assets/keydata/chicken.svg'
+import apple from '../../assets/keydata/apple.svg'
+import cheeseburger from '../../assets/keydata/cheeseburger.svg'
+
+import styles from '../../styles/Keydata.module.css'
 
 export default function Keydata({ calorieCount, proteinCount, carbohydrateCount, lipidCount }) {
     return (
         <>
-            <div>
-                <Count name="Calories" value={calorieCount} unit="kCal"></Count>
+            <div className={styles.keydata}>
+                <Count icon={energy} name="Calories" cat="calorie" value={calorieCount} unit="kCal"></Count>
+                <Count icon={chicken} name="Proteines" cat="protein" value={proteinCount} unit="g"></Count>
+                <Count icon={apple} name="Glucides" cat="carbohydrate" value={carbohydrateCount} unit="g"></Count>
+                <Count icon={cheeseburger} name="Lipides" cat="lipid" value={lipidCount} unit="g"></Count>
             </div>
         </>
     )
