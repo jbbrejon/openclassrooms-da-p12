7cdbe691751a7f9ebea30be9aeb92a4175bac1b3
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-02-01 11:55:25 +0100

    :construction: minor changes

--- src/api/fetchData/index.js
+++ src/api/fetchData/index.js
@@ -1,12 +1,70 @@
+import Data from "../../common/Data"
+
 
 export default async function fetchData(id) {
-    const baseUrl = "http://localhost:3000/"
-    try {
-        const response = await fetch(`${baseUrl}User/${id}`)
-        const { data } = await response.json()
-        return data
-    } catch (err) {
-
-        return `Error: ${err}`
+    const baseUrl = `http://localhost:3000/User/${id}`
+    async function fetchUserData() {
+        try {
+            const response = await fetch(`${baseUrl}`)
+            const { data } = await response.json()
+            return data
+        } catch (err) {
+            return `Error: ${err}`
+        }
     }
+
+    async function fetchActivityData() {
+        try {
+            const response = await fetch(`${baseUrl}/activity`)
+            const { data } = await response.json()
+            return data
+        } catch (err) {
+            return `Error: ${err}`
+        }
+    }
+
+    async function fetchAverageData() {
+        try {
+            const response = await fetch(`${baseUrl}/average-sessions`)
+            const { data } = await response.json()
+            return data
+        } catch (err) {
+            return `Error: ${err}`
+        }
+    }
+
+    async function fetchPerformanceData() {
+        try {
+            const response = await fetch(`${baseUrl}/performance`)
+            const { data } = await response.json()
+            return data
+        } catch (err) {
+            return `Error: ${err}`
+        }
+    }
+
+
+
+
+    let userData = await fetchUserData()
+    let activityData = await fetchActivityData()
+    let averageData = await fetchAverageData()
+    let performance = await fetchPerformanceData()
+
+    let myData = new Data(userData, activityData, averageData, performance)
+    console.log(myData)
+    /*console.log(`
+    ${myData.firstName},
+    ${myData.todayScore},
+    ${myData.calorieCount},
+    ${myData.proteinCount},
+    ${myData.carbohydrateCount},
+    ${myData.lipidCount},
+    ${myData.activityData},
+    ${myData.averageData},
+    ${myData.performanceData}
+    `)*/
+    return myData
 }
+
+
