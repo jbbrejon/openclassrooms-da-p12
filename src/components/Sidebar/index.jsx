fea2c277f583494b0c4040eb3d747774afa3ee2f
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-02-06 21:10:46 +0100

    :sparkles: Create Sidebar component

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,28 @@
+// Import components
+import Copyright from '../Copyright'
+import Navicon from '../Navicon'
+
+//Import assets
+import meditation from '../../assets/activity/meditation.svg'
+import swimming from '../../assets/activity/swimming.svg'
+import cycling from '../../assets/activity/cycling.svg'
+import lifting from '../../assets/activity/lifting.svg'
+
+// Import styles
+import styles from './Sidebar.module.css'
+
+export default function Sidebar() {
+    return (
+        <>
+            <nav className={styles.sidebar}>
+                <div className={styles.activities}>
+                    <Navicon path={meditation}></Navicon>
+                    <Navicon path={swimming}></Navicon>
+                    <Navicon path={cycling}></Navicon>
+                    <Navicon path={lifting}></Navicon>
+                </div>
+                <Copyright></Copyright>
+            </nav>
+        </>
+    )
+}
