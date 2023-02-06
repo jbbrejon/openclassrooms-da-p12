ea104e04df2ae13ca807bd9f3aa8452c96f441ee
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-02-06 21:04:26 +0100

    :sparkles: Create Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,28 @@
+// Import Logo
+import logo from '../../assets/logo/logo.svg'
+
+// Import CSS module
+import styles from './Header.module.css'
+
+// Import Link module
+import { Link } from 'react-router-dom'
+
+function Header() {
+    return (
+        <>
+            <header className={styles.header}>
+                <img className={styles.logo} src={logo} alt="logo Sportsee" />
+                <nav className={styles.nav}>
+                    <ul className={styles.ul}>
+                        <li className={styles.li}><Link to="/">Accueil</Link></li>
+                        <li className={styles.li}>Profil</li>
+                        <li className={styles.li}>Réglage</li>
+                        <li className={styles.li}>Communauté</li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    )
+}
+
+export default Header
