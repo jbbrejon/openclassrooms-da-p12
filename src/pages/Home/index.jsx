7c8cbd27b696a54ccfd945c781e17bff5e7565ed
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-02-06 21:11:12 +0100

    :sparkles: Create Home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+// Import CSS module
+import styles from './Home.module.css'
+
+// Import Link module
+import { Link } from 'react-router-dom'
+
+function Home() {
+
+    return (
+        <>
+            <div>Home</div>
+        </>
+    )
+}
+
+export default Home
