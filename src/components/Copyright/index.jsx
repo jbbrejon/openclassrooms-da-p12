6df4a0e4ecf59fe2cdfc0cd78515fe1c41bb19d0
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-02-06 21:09:38 +0100

    :sparkles: Create Copyright component

---
+++ src/components/Copyright/index.jsx
@@ -0,0 +1,14 @@
+// Import styles
+import styles from './Copyright.module.css'
+
+/**
+ * Copyright
+ * @component Copyright information 
+ */
+export default function Copyright() {
+    return (
+        <>
+            <small className={styles.copyright}>Copyright, SportSee 2020</small>
+        </>
+    )
+}
