fa6dba0c2d91e80d6aab8d028c1ec22fc4ff7f50
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-02-06 21:12:00 +0100

    :sparkles: Create Error component

---
+++ src/components/Error/index.jsx
@@ -0,0 +1,13 @@
+// Import CSS module
+import styles from './Error.module.css'
+
+function Error() {
+
+    return (
+        <div>
+            <h1>Oups 🙈 Cette page n'existe pas</h1>
+        </div>
+    )
+}
+
+export default Error
