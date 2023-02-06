e404a5dd6c8f1b290e75648659f1477f3f61bbf6
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-02-06 22:05:21 +0100

    :sparkles: Create Greetings component

---
+++ src/components/Greetings/index.jsx
@@ -0,0 +1,21 @@
+// Import styles
+
+import styles from './styles/Greetings.module.css'
+
+import PropTypes from 'prop-types'
+
+function Greetings({ firstName }) {
+    return (
+        <><div className={styles.greetings}>
+            <h1 className={styles.h1}>Bonjour <span className={styles.name}>{firstName}</span></h1>
+            <div className={styles.congrats}>Félicitation ! Vous avez explosé vos objectifs hier 👏</div>
+        </div>
+        </>
+    )
+}
+
+Greetings.propTypes = {
+    firstName: PropTypes.string.isRequired,
+};
+
+export default Greetings
