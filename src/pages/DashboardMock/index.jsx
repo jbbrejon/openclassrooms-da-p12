7f4da9bd89714cb95535093a3e4d0daacd20af86
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-02-07 11:21:11 +0100

    :sparkles: Create DahsboardMock component

---
+++ src/pages/DashboardMock/index.jsx
@@ -0,0 +1,52 @@
+
+// Import CSS module
+import styles from './Dashboard.module.css'
+
+
+import Greetings from '../../components/Greetings';
+import Keydata from '../../components/Keydata';
+import Activity from '../../components/Activity';
+import AverageSessions from '../../components/AverageSessions';
+import Performance from '../../components/Performance';
+import Score from '../../components/Score';
+
+import mockData from '../../api/mockData'
+
+function DashboardMock() {
+    const userData = mockData()
+    return (
+
+
+        <div className={styles.dashboard}>
+            <Greetings firstName={userData.firstName} ></Greetings>
+            <div className={styles.stats}>
+                <div className={styles.charts}>
+                    <div className={styles.activityData}>
+                        <Activity activityData={userData.activityData}></Activity>
+                    </div>
+                    <div className={styles.row}>
+                        <div className={styles.box}>
+                            <AverageSessions averageData={userData.averageData} ></AverageSessions>
+                        </div>
+                        <div className={styles.box}>
+                            <Performance performanceData={userData.performanceData}></Performance>
+                        </div>
+                        <div className={styles.box}>
+                            <Score scoreData={userData.todayScore}></Score>
+                        </div>
+                    </div>
+                </div>
+                <Keydata
+                    className={styles.keydata}
+                    calorieCount={userData.calorieCount}
+                    proteinCount={userData.proteinCount}
+                    carbohydrateCount={userData.carbohydrateCount}
+                    lipidCount={userData.lipidCount}
+                >
+                </Keydata>
+            </div>
+        </div>
+    )
+}
+
+export default DashboardMock
