aa0c1543f8d8476ec2e1fce3814b370dbd84aa0b
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-02-07 11:20:25 +0100

    :sparkles: Create Performance component

---
+++ src/components/Performance/index.jsx
@@ -0,0 +1,52 @@
+// Recharts 
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, ResponsiveContainer } from 'recharts';
+
+
+
+// Import styles
+import styles from './Performance.module.css'
+
+import PropTypes from 'prop-types'
+
+
+function Performance({ performanceData }) {
+    console.log(performanceData)
+    return (
+        <>
+            <div className={styles.statbox}>
+
+
+                <ResponsiveContainer width="100%" height="100%">
+                    <RadarChart
+                        cx="50%"
+                        cy="50%"
+                        outerRadius="65%"
+                        data={performanceData}
+                        style={{ backgroundColor: '#282D30' }}
+                    >
+                        <PolarGrid stroke="white" />
+                        <PolarAngleAxis
+                            stroke="white"
+                            dataKey="kindName"
+                            tick={{ fontSize: '10px' }}
+                            tickLine={false}
+                        />
+                        <Radar
+                            name="Performance"
+                            dataKey="value"
+                            stroke="#e60000"
+                            fill="#e60000"
+                            fillOpacity={0.6}
+                        />
+                    </RadarChart>
+                </ResponsiveContainer>
+            </div>
+        </>
+    )
+}
+
+Performance.propTypes = {
+    performanceData: PropTypes.array.isRequired,
+};
+
+export default Performance
